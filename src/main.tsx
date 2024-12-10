610206169d0a4c69ce09e372ce8d80705f3fd4e4
moshe-stern <guntheramassed@gmail.com>
2024-12-10 16:01:11 -0500

    builds

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./components/App.tsx";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { msalConfig } from "./authConfig.ts";
