963c01c0c3310652a92dc99112a2d88d258be3a6
Moshe Stern <guntheramassed@gmail.com>
2024-12-11 21:12:08 -0500

    css

--- src/components/cubes.tsx
+++ src/components/cubes.tsx
@@ -17,26 +17,28 @@ export default function Cubes() {
   }, []);
   return (
     <>
-        <h1>Cube Refresh Times</h1>
+      <div className="jumbotron">
+        <h1 className="display-3">Cube Refresh Times</h1>
         <Link to={"/message-board"}>Message board</Link>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Load Time</th>
-              <th>Pipeline Name</th>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Load Time</th>
+            <th>Pipeline Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cubes.map((cube, index) => (
+            <tr key={index} className={index % 2 !== 0 ? 'table-primary' : ''}>
+              <td>{cube.name}</td>
+              <td>{cube.loadTime || ""}</td>
+              <td>{cube.pipelineName || ""}</td>
             </tr>
-          </thead>
-          <tbody>
-            {cubes.map((cube, index) => (
-              <tr key={index} className={index % 2 !== 0 ? 'table-primary' : ''}>
-                <td>{cube.name}</td>
-                <td>{cube.loadTime || ""}</td>
-                <td>{cube.pipelineName || ""}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
--- src/components/message-board.tsx
+++ src/components/message-board.tsx
@@ -8,8 +8,10 @@ export default function MessageBoard(prop: IMessageBoardProp) {
   return (
     <>
       <div className="message-board">
-        <h1>Message Board</h1>
-        <Link to={'/'}>Cube Refresh Times</Link>
+        <div className="jumbotron">
+          <h1 className="display-3">Message Board</h1>
+          <Link to={'/'}>Cube Refresh Times</Link>
+        </div>
         {prop.user && prop.user.authorityType
           ? `You can edit ${prop.user.authorityType}`
           : "You can not edit"}
